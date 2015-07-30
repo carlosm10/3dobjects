71541044231adf64f9f42e3b3529749df4230cee
Carlos Mondragon <carlosmondragon10@gmail.com>
2015-07-30 17:58:07 -0500

    Objects Rotating

--- Assets/Cardboard/DemoScene/Rotate.cs
+++ Assets/Cardboard/DemoScene/Rotate.cs
@@ -7,37 +7,25 @@ public class Rotate : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//SetGazedAt(false);
-		gazedAt = false;
-
-
+		//gazedAt= false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//var rot = Cardboard.SDK.HeadPose.Orientation;
+		/*if (gazedAt) {
+			RotateOn();
+		}*/
 
-		if (gazedAt) {
-			RotateOn ();
-		}
-
-	}
-
-	public void SetGazedAt() {
-		//GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
-		if(gazedAt=false){
-			gazedAt = true;
-		}else{
-			gazedAt = false;
-		}
-	}
-
-	public void RotateOn(){
 		var rot = Cardboard.SDK.HeadPose.Orientation;
 		transform.localRotation = rot;
 	}
+	/*
+	public void Selected(){
+		gazedAt = true;
+		//Debug.Log( GameObject.Find("Pic").transform.position);
+	}
 
+	public void RotateOn(){
 
-
-
+	}*/
 }
